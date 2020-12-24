48587d413b5530c01c13f4eb795397c6bf66c082
sunhu <1125245923@qq.com>
2020-12-24 10:03:43 +0800

    项目框架搭建

---
+++ passjava-channel/src/test/java/com/sh/passjava/channel/PassjavaChannelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sh.passjava.channel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassjavaChannelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
